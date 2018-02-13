741ef30acdd5c791e3e4f293f503fb6a25e89747
Kenneth <kenny.selorm@gmail.com>
2018-02-13 10:27:21 +0000

    now can delete user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   belongs_to :department   
   has_one :sector, through: :department
 
-  has_many :activities 
+  has_many :activities, dependent: :destroy	 
   
 
 end
