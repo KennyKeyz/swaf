f567d3d3bfc855f951f4f1e715f76429018fa98d
Kenneth <kenny.selorm@gmail.com>
2018-02-26 15:54:41 +0000

    mailchimp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://activityform.herokuapp.com' }
+
+    config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.mandrillapp.com",
+   :port                 => 587,
+   :user_name            => "candidkenny",
+   :password             => "Kenneth$el0rm",
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
+  
 end
