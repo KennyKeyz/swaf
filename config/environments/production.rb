4f501f29fb058d8d0cf765def8454fc0760ab7d3
Kenneth <kenny.selorm@gmail.com>
2018-02-26 15:21:56 +0000

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "www.activityform.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: 'http://activityform.herokuapp.com' }
 end
