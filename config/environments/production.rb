efd2d725f7b52613183c0e336fbabfd720bf2f4f
Kenneth <kenny.selorm@gmail.com>
2018-02-26 23:17:51 +0000

    mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
-    :port      => 25,
+    :port      => 587,
     :enable_starttls_auto => true,
     :user_name => ENV["MANDRILL_USERNAME"],
     :password  => ENV["MANDRILL_API_KEY"],
